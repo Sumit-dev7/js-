851796442a772cf3d6f83b8d6d0e932a67ef3cea
Sumit-dev7 <sumitkumarpanda94@gmail.com>
2025-10-18 20:54:05 +0530

    commit

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const myVariable = 5;
+let anotherVariable = 'Hello, World!';
+var oldStyleVariable = true;
+accessorVariable = 42;
+//prefer to not use 'var' in modern JavaScript
+
+console.log(myVariable);
+console.table([anotherVariable, oldStyleVariable, accessorVariable]);
